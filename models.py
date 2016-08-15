d82aa703e0852f048e869cbc2b1b7c04d92676d5
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-15 16:12:44 -0300

    First try at rank calculation

--- models.py
+++ models.py
@@ -16,9 +16,20 @@ _logger = logging.getLogger(__name__)
 class product_product(models.Model):
 	_inherit = 'product.product'
 
-	@api.one
-	def _compute_product_rank(self):
+	@api.multi
+	def _update_product_rank(self):
 		return_value = 0
+		import pdb;pdb.set_trace()
+		previous_date = date.today() - timedelta(days=365)
+		invoices = self.env['account.invoice'].search([('date_invoice','>=',previous_date),
+			('state','in',['open','paid'])])
+		product_amount = {}
+		for invoice in invoices:
+			for invoice_line in in invoice.invoice_line:
+				if invoice_line.product_id.id not in product_amount.keys():
+					product_amount[invoice_line.product_id.id] = invoice_line.price_subtotal
+				else:
+					product_amount[invoice_line.product_id.id] += invoice_line.price_subtotal
 		self.product_rank = return_value
 
-	product_rank = fields.Integer('Ranking',compute=_compute_product_rank)
+	product_rank = fields.Integer('Ranking')
