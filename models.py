6384f288f02e9251dc9b7c273b7058ceb957ecb2
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-15 16:37:09 -0300

    Fixed bug0

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ import openerp.addons.decimal_precision as dp
 import logging
 import datetime
 from openerp.fields import Date as newdate
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from dateutil import relativedelta
 #Get the logger
 _logger = logging.getLogger(__name__)
@@ -20,7 +20,7 @@ class product_product(models.Model):
 	def _update_product_rank(self):
 		return_value = 0
 		import pdb;pdb.set_trace()
-		previous_date = datetime.date.today() - timedelta(days=365)
+		previous_date = date.today() - timedelta(days=365)
 		invoices = self.env['account.invoice'].search([('date_invoice','>=',previous_date),
 			('state','in',['open','paid'])])
 		product_amount = {}
