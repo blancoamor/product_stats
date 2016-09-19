0ff2f94c0c72498b315c6e4d448971a22e1f9e20
morrillo <gustavo.orrillo@moldeo.coop>
2016-09-19 18:19:53 -0300

    Added fields to wizard

--- wizard/wizard_model.py
+++ wizard/wizard_model.py
@@ -11,8 +11,15 @@ from openerp import models, fields, api
 class product_generate_abastecimiento(models.TransientModel):
 	_name= 'product.generate.abastecimiento'
 
+	warehouse_id = fields.Many2one('stock.warehouse',string='Almacen')
+	location_id = fields.Many2one('stock.location',string='Almacen')
+
 	@api.multi
 	def generate_abastecimiento(self):
 		import pdb;pdb.set_trace()
+		context = self.env.context
+		if context['active_model'] == 'product.product':
+			for active_id in context['active_ids']:
+				product = self.env['product.product'].browse(active_id)
 		return None		
 
